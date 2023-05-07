d8fc4582891b550fc071c189269e480842cb7133
SK SOHEL RAHAMAN <sohelrahaman43@gmail.com>
2023-05-07 13:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function play() { 
+     var audio = document.getElementById("audio"); 
+     audio.play(); 
+   } 
+ function play2(){ 
+         var audio = document.getElementById("audio2"); 
+     audio.play2(); 
+ }
